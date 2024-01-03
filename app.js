47072462ad22cc72b61bc7521203a726878048a8
stylish_code <minyoung0101q@gmail.com>
2024-01-03 19:31:43 +0900

    form submission 다음 커밋: form 안의 input 에서 form은 자동으로 submit 됨 이를 막아줌

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
 
-function onLoginBtnClick() {
-  console.log("hello", loginInput.value);
+function onLoginSubmit() {
+  const username = loginInput.value;
+  console.log(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
