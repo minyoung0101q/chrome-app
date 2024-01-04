054da16709b87dee805784f87b3b2eb3fb94f09f
stylish_code <minyoung0101q@gmail.com>
2024-01-05 00:54:28 +0900

    saving username

--- app.js
+++ app.js
@@ -2,20 +2,13 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-/* 
-관습 같은 것이다. -> 일반적으로 string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용한다
-그리고 이건 loginForm이나 loginInput 처럼 중요한 정보를 담은게 아니라서 대문자로 작성한다
-*/
-
-const HIDDEN_CLASSNAME = "hidden";
-
-function onLoginSubmit(event) {
+function handleFormSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginForm.classList.add("hidden");
+  greeting.classList.remove("hidden");
   const username = loginInput.value;
-  greeting.innerText = "Hello " + username;
+  localStorage.setItem("username", username);
   greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+loginForm.addEventListener("submit", handleFormSubmit);
