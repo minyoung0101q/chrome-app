aaba0947d8e165b725fa864a1fcb5a980d589f62
stylish_code <minyoung0101q@gmail.com>
2024-01-05 01:44:38 +0900

    loading username

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-function handleFormSubmit(event) {
+console.log(loginForm);
+console.log(loginInput);
+console.log(greeting);
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function handleLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add("hidden");
-  greeting.classList.remove("hidden");
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+// username을 인자로 받는 함수
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", handleFormSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+/* localStorage에 유저정보가 없을 때
+우리는 form의 submit을 기다린다.
+form이 submit 되면 우리는 input으로부터 유저정보를 받고, 
+ */
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLoginSubmit);
+  // localStorage에 유저정보가 있을 때
+} else {
+  paintGreetings(savedUsername);
+}
