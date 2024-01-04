6170bd5d5905101deb1a3537e992211a5b1d9bba
stylish_code <minyoung0101q@gmail.com>
2024-01-05 02:59:50 +0900

    Timeouts and Dates - timeouts => intervals와 timeouts 정리

--- js/clock.js
+++ js/clock.js
@@ -4,4 +4,6 @@ function sayHello() {
   console.log("hello");
 }
 
-setInterval(sayHello, 2000);
+setTimeout(sayHello, 2000);
+//setTimeout : 2초 뒤에 hello가 로그에 출력됨
+//Intervals : 처음에 아무것도 출력되지 않고 2초 후에 hello 출력 -> 이게 반복
