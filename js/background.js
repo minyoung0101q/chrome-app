8b0e7ee02ec16689d3b3705f13f83e27e8f85df6
stylish_code <minyoung0101q@gmail.com>
2024-01-06 04:16:54 +0900

    recap - prepend()

--- js/background.js
+++ js/background.js
@@ -1,13 +1,14 @@
+//먼저 images 배열을 생성한다.
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-console.log(chosenImage); //2.jpeg 0.jpeg 등 랜덤으로 나옴
-//Javascript에서 이미지를 만들고 그걸 html에 추가하자
-const bgImage = document.createElement("img"); //img라는 element를 생성해보자
-console.log(bgImage); //<img>
-//백틱(``)으로 바꾸고 chosenImage를 추가해서 string을 만들자
-bgImage.src = `img/${chosenImage}`;
-//다시 bgImage 콘솔을 찍어보자
-console.log(bgImage);
+//images 배열에서 random으로 꺼낸다.
+const randomImage = images[Math.floor(Math.random() * images.length)];
+console.log(randomImage); //2.jpeg 아니면 0.jpeg 등 랜덤 이걸 src 경로에 넣을것
 
-document.body.appendChild(bgImage);
+//body에 createElement해서 넣어준다./
+//먼저, createElement한다.
+const image = document.createElement("img");
+console.log(image); //<img>
+//appendChild()
+image.src = `img/${randomImage}`;
+document.body.prepend(image);
