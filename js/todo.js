82971025ef12d33300b6d5d3495b78472ef2eb08
stylish_code <minyoung0101q@gmail.com>
2024-01-06 23:17:28 +0900

    recap

--- js/todo.js
+++ js/todo.js
@@ -1,42 +1,35 @@
-//form과 ul을 HTML에서 JavaScript로 가져오기
-const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input"); //document 대신에 toDoForm 활용
-const toDoList = document.getElementById("todo-list");
-console.log(greetings); //greeting.js 파일에서 정의한 greetings를 쓸 수 있음
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoUl = document.querySelector("#todo-list");
+console.log(toDoUl); // <ul></ul>
 
-function deleteToDo(event) {
-  console.dir(event.target.parentElement.innerText); //innerText 정보를 얻을 수 있음
-  const li = event.target.parentElement;
-  li.remove(); //이게 다다, 이렇게하면 삭제 된다.
+function handleDeleteToDo(event) {
+  const li = event.target.parentElement; // button의 부모인 <li> <span>dd</span> <button>X</button> </li>
+  li.remove();
 }
 
-//toDo를 그리는 역할
-//handleToDoForm function이 paintToDo를 사용하게 됨
-function paintToDo(newTodo) {
-  console.log("I will paint", newTodo);
+function paintToDo(newToDo) {
+  // li, span 생성
   const li = document.createElement("li");
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newToDo;
+  // button 생성
   const button = document.createElement("button");
   button.innerText = "X";
-  button.addEventListener("click", deleteToDo);
-  //두 element를 만들었다, 이제 span은 li 내부에 있어야 한다.
-  li.appendChild(span); //이제 li는 span이라는 자식을 갖게 되었다.
-  li.appendChild(button);
-
+  button.addEventListener("click", handleDeleteToDo);
+  li.appendChild(span); // <span></span>
+  console.log(li); // <li> <span></span> </li>
+  li.appendChild(button); // appendChild() 순서 지키기
   console.log(li);
-  //이제 이 새로운 li를 toDoList에 추가해야 한다. 4줄의 toDoList!!
-  toDoList.appendChild(li);
+  toDoUl.appendChild(li);
+  console.log(toDoUl); // <ul> <li> <span></span> </li> </ul>
 }
-
-//greetings의 form에서 했던 것과 비슷한 로직
 
 function handleToDoForm(event) {
   event.preventDefault();
-  //todolist의 첫번째 단계
-  const newTodo = toDoInput.value; //input의 value를 비우기 전에 나타내는 string
+  const newToDo = toDoInput.value;
   toDoInput.value = "";
-  paintToDo(newTodo);
+  paintToDo(newToDo);
 }
-//submit eventListener
+
 toDoForm.addEventListener("submit", handleToDoForm);
