6cf542edf7226951ea2b80f210a280f01243553e
stylish_code <minyoung0101q@gmail.com>
2024-01-08 14:25:45 +0900

    deleting to dos part three - complete

--- js/todo.js
+++ js/todo.js
@@ -11,8 +11,17 @@ function saveToDos() {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
-  console.log(li.id); //삭제하기 전에 li의 id를 얻는다.
   li.remove();
+  console.log(li.id); //삭제하기 전에 li의 id를 얻는다.
+  //toDo는 toDos DB에 있는 요소 중 하나이다.
+  //그래서 이 함수는 DB에 있는 모든 것과 함께 실행된다.
+  //이 문장의 의미 : 우리카 클릭한 li.id와 다른 toDo는 남겨두고 싶다.
+  //li.id는 string 타입이고, toDo.id는 number 타입이다. => 그래서 이 둘이 다른 게 되니까 아무것도 지워지지 않는다.
+  //parseInt는 문자열을 숫자로 바꾼다.
+  //filter는 새로운 배열을 반환한다. 조건을 true하는 것들만 모아서
+  //클릭한 li의 id와 db의 id가 같지 않은 것들을 모아 새로운 배열을 반환한다.
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos(); //그리고 그것을 localStorage에 저장해서 반영한다.
 }
 
 function paintToDo(toDoInputValue) {
